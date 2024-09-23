e53a46f332ff2af2b105e17cef2a3f72c281b15e
isaacmartin <isaacissamartin@gmail.com>
2024-09-23 14:05:01 -0400

    Completed portfolio

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap5
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.secret_key = "1234"
 Bootstrap5(app)
 
+
 class ContactForm(FlaskForm):
-    first_name = StringField(label="First Name", validators=[DataRequired()])
-    last_name = StringField(label="Last Name", validators=[DataRequired()])
+    name = StringField(label="Name", validators=[DataRequired()])
     email_address = StringField(label='Email Address', validators=[DataRequired()])
-    message = StringField(label='Message', validators=[DataRequired()])
+    message = TextAreaField(label='Message', validators=[DataRequired()])
     subject = StringField(label='Subject', validators=[DataRequired()])
     submit = SubmitField(label="Send")
 
@@ -21,7 +21,7 @@ class ContactForm(FlaskForm):
 def homepage():
     contact_form = ContactForm()
     if contact_form.validate_on_submit():
-        print(contact_form.first_name.data)
+        print(contact_form.name.data)
     return render_template("index.html", form=contact_form)
 
 
